414943475407c33c0314ffad4f604b5b82421e51
Shiva Kumar <shiva@Shivas-MacBook-Air.local>
2021-10-03 10:43:32 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    shared_credentials_file = "~/.aws/credentials"
+    region = "ap-south-1"
+
+}
+
+# resource "aws_s3_bucket" "test_s3" {
+#   bucket = "demo-21-bucket-terraform"
+#   acl    = "private"
+# }
+
+#  resource "aws_ec2_host" "test" {
+#   instance_type     = "c5.18xlarge"
+#   availability_zone = "ap-south-1a"
+#   host_recovery     = "on"
+#   auto_placement    = "on"
+# }
+
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
+}
+
+variable "environment" {
+    description = "identify the env"
+  
+}
+
+resource "aws_vpc" "development_vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: var.environment
+    }
+}
+
+# resource "aws_subnet" "development-subnet-1" {
+#     vpc_id = aws_vpc.development_vpc.id
+#     cidr_block = "10.0.10.0/24"
+#     availability_zone = "ap-southeast-2e"
+#     map_public_ip_on_launch  = false
+#     assign_ipv6_address_on_creation = false
+# }
+
+# data "aws_vpc" "existing_vpc"{
+#     default = true
+# }
+
+
---
+++ terraform-dev.tfvars
@@ -0,0 +1,2 @@
+vpc_cidr_block = "10.0.20.0/24"
+environment = "development"
